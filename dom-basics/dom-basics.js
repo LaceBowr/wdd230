174ef65a8b758d6166669c3f2347ae464684072d
Laycie Bowers <layciebowers@gmail.com>
2022-01-15 21:51:01 -0800

    design principles

---
+++ dom-basics/dom-basics.js
@@ -0,0 +1,26 @@
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+
+
+let myImage = document.createElement("img");
+myImage.setAttribute("src", "sweetHeart.jpg");
+myImage.setAttribute("alt", "Sweet-tart heart cupcake candle");
+myImage.setAttribute("width", "200px");
+myImage.setAttribute("height", "200px");
+document.body.appendChild(myImage);
+
+const heading2 = document.createElement("h2");
+heading2.innerText = "Seciton 3";
+document.body.appendChild(heading2);
+
+const newSection = document.createElement("section");
+newSection.innerText = "This is Section 3 paragraph";
+document.body.appendChild(newSection);
+
+const javaParagraph = document.createElement("p");
+javaParagraph.innerText = "Welcome to Javascript Language";
+document.body.appendChild(javaParagraph);
+
+
+
