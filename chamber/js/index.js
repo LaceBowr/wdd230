2039f3f2794cc5e6d280ba5dbe694b5e01224e6d
Laycie Bowers <layciebowers@gmail.com>
2022-02-05 22:40:20 -0800

    homepage week 6

--- chamber/js/index.js
+++ chamber/js/index.js
@@ -14,3 +14,11 @@ const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+const hambutton = document.querySelector(".menu");
+const mainnav = document.querySelector(".ham");
+hambutton.addEventListener("click", () =>
+{maninnav.classList.toggle("responsive")}, false);
+
+//to solve the mid resizing issue with responsive class on 
+window.onresize = () => {of (window.innerWidth >760)
+mainnav.classList.remove("responsive")};
