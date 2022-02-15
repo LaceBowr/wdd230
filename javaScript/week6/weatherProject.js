b809fe866406235de917a0797cc44751c5729236
Laycie Bowers <layciebowers@gmail.com>
2022-02-14 21:27:26 -0800

    presentation

--- javaScript/week6/weatherProject.js
+++ javaScript/week6/weatherProject.js
@@ -1,54 +1,63 @@
 
-let currentDate = new Date();
-let currentDay = date.getDay();
-let currentDateDay = day + date;
-let apiInfo = "http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={f59a4105de49f60386b736f3f818131c}";
-document.getElementById("date").innerHTML = dateTime;
-document.getElementById("dayOfWeek").innerHTML = day;
-document.getElementByID("cityStateCountry").innerHTML = cityStateCountry; 
+
+//let currentDateDay = day + date;
+const date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format();
+//const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
+// long, medium, short options ... try them
+date.innerHTML = `<em>${date}</em>`;
+//dayOfWeek.innerHTML = `<em>${fulldateUK}</em>`;
+document.getElementById("date").innerHTML = date;
+//document.getElementById("dayOfWeek").innerHTML = date.day;
+
+
 function moveCurrentDateAndDayToGenerate(){
     
-    document.querySelector("#date").textContent = today;
-    document.querySelector("#dayOfWeek").textContent = today;
-    
-    return today
+    let today = document.querySelector("#date").textContent ;
+    let day = document.querySelector("#dayOfWeek").textContent;
+    let place = document.querySelector("#cityStateCountry").value;
+    getWeather(place).then(fillData).then(getIconInfo).then(calulateWindChill);
 }
-document.querySelector("#cityStateCountry").addEventListener
+document.querySelector("#moveCurrentDateAndDay").addEventListener("click", moveCurrentDateAndDayToGenerate)
 
-async function getWeather(){
+async function getWeather(city){
+    let apiInfo = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=f59a4105de49f60386b736f3f818131c`;
     const response = await fetch(apiInfo);
-    data = await responses.json()
+    data = await response.json()
     return data
 }
 function fillData(){
-    newTemp = data["main"]["temp"]
-    speed = data["wind"]["speed"]
+    let newTemp = data["main"]["temp"];
+    let speed = data["wind"]["speed"];
 
-    let currentTemp = Math.round(newTemp)
-    localtemp.textContent += `${currentTemp}`;
+    let currentTemp = Math.round(newTemp);
+    let localtemp = document.querySelector("#temp");
+    localtemp.textContent = `${currentTemp}`;
 
-    let currentspeed = Math.round(speed)
-    wSpeed.textContent = `${currentspeed}mph`
+    let currentspeed = Math.round(speed);
+    let wSpeed = document.querySelector("#windspeed");
+    wSpeed.textContent = `${currentspeed}`;
 
-    return newTemp, speed
+    let windChill = calulateWindChill(currentspeed, currentTemp);
+    let wChill = document.querySelector("#windchill");
+    wChill.textContent = windChill;
+
+    return newTemp, speed;
 }
 function getIconInfo(){
     let icon = data["weather"][0]["icon"];
-    let desc = data["weather"][0]["info"];
-
+    let desc = data["weather"][0]["description"];
+    document.querySelector("#weatherIcon");
     weatherIcon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
     weatherIcon.alt = `open weather map API icon: ${desc}`;
-    iconInfo.textContent = info.toUpperCase()
+    document.querySelector("#iconDesc").innerHTML = desc;
 }
 
-function calulateWindChill() {
-    if (newTemp <= 50 && speed > 1.0){
-        let chill = 35.74 + (0.6215*newTemp)-(35.75*speed**0.16)
+function calulateWindChill(speed, temp) {
+    if (temp <= 50 && speed > 1.0){
+        let chill = 35.74 + (0.6215*temp)-(35.75*speed**0.16)
         chill = Math.round(chill)
-        calulateWindChill.innerHTML = `Feels like ${chill} &#176;F`
+        return chill;
     }else{
-        calulateWindChill.textContent = `N/A`
+        return `N/A`;
     }
 }
-
-getWeather().then(fillData).then(getIconInfo).then(calculateWindChill);
