2b18b0a933579e6dc015a22887685f3f0561adf9
Laycie Bowers <layciebowers@gmail.com>
2022-02-19 16:56:23 -0800

    items forgotten

--- chamber/js/index.js
+++ chamber/js/index.js
@@ -1,4 +1,3 @@
-// select the elements to manipulate (output to)
 const datefield = document.querySelector("date");
 const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
 
@@ -17,8 +16,64 @@ datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
 const hambutton = document.querySelector(".menu");
 const mainnav = document.querySelector(".ham");
 hambutton.addEventListener("click", () =>
-{maninnav.classList.toggle("responsive")}, false);
+{mainnav.classList.toggle("responsive")}, false);
 
 //to solve the mid resizing issue with responsive class on 
 window.onresize = () => { (window.innerWidth >760)
 mainnav.classList.remove("responsive")};
+
+//get all imgs with data-src attribute
+const images = document.querySelectorAll("[data-src]");
+
+//optional parameters being set for the IntersectionalObserver
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 0px 0px"
+};
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if(!src) {
+        return;
+    }
+    img.src = src;
+}
+//const loadImages = (image) => {
+//    image.setAttribute("src", image.getAttribute("data-src"));
+//    image.onload = () => {image.removeAttribute("data-src");};
+//};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+//first check to see if Intersenction Observer is supported
+entries.forEach(entry => {
+    if(!entry.isIntersecting) {
+        return; 
+    } else { // just load images if not supported....
+        preloadImage(entry.target);
+        imgObserver.unobserve(entry.target);
+        }
+    });
+}, imgOptions);
+
+
+images.forEach(image => {
+    imgObserver.observe(image);
+});
+
+
+const visitsDisplay = document.querySelector(".visits");
+//get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+//determine if this is the first visit or display the number of visits.
+if(numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+}else{
+    visitsDisplay.textContent = `This is your first visit!`;
+}
+//increment the number of visits.
+numVisits++;
+//store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+//show todays date.
+//todayDisplay.textContent = Date.now();
