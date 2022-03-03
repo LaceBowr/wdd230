80b54170d7315749180e9a5036e4180d9cc02ae4
Laycie Bowers <layciebowers@gmail.com>
2022-03-02 20:30:58 -0800

    prophetsLarge page

--- lesson9/prophets/prophets.js
+++ lesson9/prophets/prophets.js
@@ -15,21 +15,32 @@ fetch(requestURL)
     {
         // Create elements to add to the document
         let card = document.createElement('section');
+        card.setAttribute('class','grid');
         let h2 = document.createElement('h2');
         let portrait = document.createElement('img');
         let h3 = document.createElement('h3');
         let h4 = document.createElement('h4');
+        let order = '';
         
       
         // Change the textContent property of the h2 element to contain the prophet's full name
-        h2.textContent = prophet.name + ' ' + prophet.lastname;
-        h3.textContent = prophet.birthdate;
-        h4.textContent = prophet.birthplace;
+        h2.textContent = ` ${prophet.name} ${prophet.lastname}`;
+        h3.innerHTML = `Birthdate: <Strong>${prophet.birthdate}<Strong>`;
+        h4.textContent = `Birthplace: ${prophet.birthplace}`;
         // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
         portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
+        portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
         portrait.setAttribute('loading', 'lazy');
       
+        if (prophet.order === 1){
+          order = `${prophet.order}st`;
+        } else if (prophet.order === 2){
+          order = `${prophet.order}nd`;
+        } else if (prophet.order === 3){
+          order = `${prophet.order}rd`;
+        } else {
+          order = '${prophet.order}th';
+        }
         // Add/append the section(card) with the h2 element
         card.appendChild(h2);
         card.appendChild(portrait);
